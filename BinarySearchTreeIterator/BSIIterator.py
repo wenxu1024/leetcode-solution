e473b145e63653f911f3ced126c60e689a442aad
dragonrider1024 <xuzhiwen1024@gmail.com>
2015-01-03 13:06:51 -0500

    Binary Search Tree Iterator

--- BinarySearchTreeIterator/BSIIterator.py
+++ BinarySearchTreeIterator/BSIIterator.py
@@ -1,6 +1,4 @@
-#! /usr/bin/python
-
-# Definition for a  binary tree node
+#definition for a  binary tree node
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
@@ -10,14 +8,33 @@
 class BSTIterator:
     # @param root, a binary search tree's root node
     def __init__(self, root):
+        self.l = []
+        current = root
+        while current != None:
+            self.l.append(current)
+            current = current.left
         
 
     # @return a boolean, whether we have a next smallest number
     def hasNext(self):
-        
+        if self.l == []:
+            return False
+        else:
+            return True
 
     # @return an integer, the next smallest number
     def next(self):
+        current = self.l[-1]
+        self.l.pop()
+        p = current
+        if p.right != None:
+            p = p.right
+            while p != None:
+                self.l.append(p)
+                p = p.left
+        return current.val
+            
+            
         
 
 # Your BSTIterator will be called like this:
