efd9ced34b357e98bf86eb6e76c1711fb4108c61
dragonrider1024 <xuzhiwen1024@gmail.com>
2015-01-27 12:07:39 -0500

    Surrounded Regions, DFS from the border instead of the center

--- surrounded_regions/sregions.py
+++ surrounded_regions/sregions.py
@@ -1,63 +1,63 @@
 #! /usr/bin/python
+
+        
+   
 class Solution:
     # @param board, a 9x9 2D array
     # Capture all regions by modifying the input board in-place.
     # Do not return any value.
     def solve(self, board):
-        m=len(board)
-        if m==0:
-	    return
-        n=len(board[0])
+        m = len(board)
+        if m == 0:
+            return
+        n = len(board[0])
+        visited = [[False for j in range(n)] for i in range(m)]
+        for i in range(m):
+            self.dfs_solve(board, i, 0, visited, m, n)
+            self.dfs_solve(board, i, n - 1, visited, m, n)
+        for j in range(n):
+            self.dfs_solve(board, 0, j, visited, m, n)
+            self.dfs_solve(board, m - 1, j, visited, m, n)
         for i in range(m):
             for j in range(n):
-                if board[i][j]=='0' and i>0 and i<m-1 and j>0 and j<n-1:
-                    pos=[]
-                    visited=[[False for k in range(n)] for l in range(m)]
-                    flag=self.dfs_visit(i,j,board,visited,pos,m,n)
-		   # print flag
-                    if flag:
-		#	print pos
-                        for p in pos:
-                            board[p[0]][p[1]]='X'
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                else:
+                    if board[i][j] == 'Y':
+                        board[i][j] = 'O'
         return
-                    
-                    
-    def dfs_visit(self,i,j,board,visited,pos,m,n):
-        if i<=0 or i>=m-1 or j<=0 or j>=n-1:
-            return False
-        if visited[i][j]==True:
-            return True
-        visited[i][j]=True
-        pos+=[(i,j)]
-        res=True
-        if board[i+1][j]=='0':
-            res=res and self.dfs_visit(i+1,j,board,visited,pos,m,n)
-        if board[i-1][j]=='0':
-            res=res and self.dfs_visit(i-1,j,board,visited,pos,m,n)
-        if board[i][j+1]=='0':
-            res=res and self.dfs_visit(i,j+1,board,visited,pos,m,n)
-        if board[i][j-1]=='0':
-            res=res and self.dfs_visit(i,j-1,board,visited,pos,m,n)
-        return res
+        
+        
+    def dfs_solve(self, board, i, j, visited, m, n):
+        if visited[i][j]:
+            return
+        if board[i][j] == 'O':
+            visited[i][j] = True
+            board[i][j] = 'Y'
+            if i + 1 < m and board[i + 1][j] == 'O' and visited[i + 1][j] == False:
+                self.dfs_solve(board, i + 1, j, visited, m, n)
+            if i - 1 > 0 and board[i - 1][j] == 'O' and visited[i - 1][j] == False:
+                self.dfs_solve(board, i - 1, j, visited, m, n)
+            if j + 1 < n and board[i][j + 1] == 'O' and visited[i][j + 1] == False:
+                self.dfs_solve(board, i, j + 1, visited, m, n)
+            if j - 1 > 0 and board[i][j - 1] == 'O' and visited[i][j - 1] == False:
+                self.dfs_solve(board, i, j - 1, visited, m, n)
+        return
+    
+
+
 
 if __name__=='__main__':
-    board=[['' for i in range(4)] for j in range(4)]
-    board[0][0]='X'
-    board[0][1]='X'
-    board[0][2]='0'
-    board[0][3]='X'
-    board[1][0]='X'
-    board[1][1]='0'
-    board[1][2]='0'
-    board[1][3]='X'
-    board[2][0]='X'
-    board[2][1]='X'
-    board[2][2]='0'
-    board[2][3]='X'
-    board[3][0]='X'
-    board[3][1]='0'
-    board[3][2]='X'
-    board[3][3]='X'
+    board=[['' for i in range(3)] for j in range(3)]
+    board[0][0]='O'
+    board[0][1]='O'
+    board[0][2]='O'
+    board[1][0]='O'
+    board[1][1]='O'
+    board[1][2]='O'
+    board[2][0]='O'
+    board[2][1]='O'
+    board[2][2]='O'
     print board
     s=Solution()
     s.solve(board)
